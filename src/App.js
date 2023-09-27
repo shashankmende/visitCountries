244d1f67b30a04b49a5865ac783df181bba77620
Shashank <shashankmende@gmail.com>
2023-09-27 10:40:27 +0000

    visit countries

--- src/App.js
+++ src/App.js
@@ -128,6 +128,20 @@ class App extends Component {
 
   render() {
     const {vistedCountriesList} = this.state
+
+    vistedCountriesList.sort((a, b) => {
+      const nameA = a.name.toUpperCase() // ignore upper and lowercase
+      const nameB = b.name.toUpperCase() // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1
+      }
+      if (nameA > nameB) {
+        return 1
+      }
+
+      return 0
+    })
+
     console.log('filtered lst', vistedCountriesList)
     return (
       <div className="bg-container">
